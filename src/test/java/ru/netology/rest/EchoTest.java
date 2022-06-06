9620794e84d8dd53bd88ea69398213ab48be7727
Alena Fomina <alenavladimirovna@list.ru>
2022-06-07 02:01:25 +0300

    initial commit test

--- src/test/java/ru/netology/rest/EchoTest.java
+++ src/test/java/ru/netology/rest/EchoTest.java
@@ -20,7 +20,7 @@ public class EchoTest {
         // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("sume data"))
+                .body("data", equalTo("some data"))
         ;
     }
 }
